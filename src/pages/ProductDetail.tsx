e383f83a91fb63ad55893666f1a5f39859c91af9
shuraim-nms <shuraimnms597@gmail.com>
2026-01-31 01:12:18 +0530

    Remove all files - preparing frontend for clean deployment

--- src/pages/ProductDetail.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ProductDetail() {
-  return (
-    <div className="min-h-screen bg-black text-white flex items-center justify-center">
-      <h1 className="text-4xl font-bold">Product Detail Page</h1>
-    </div>
-  );
-}
